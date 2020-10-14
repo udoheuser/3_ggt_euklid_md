862c2357aafebd9941ac548906793a53818a7f93
udoheuser <heuser@dhbw-stuttgart.de>
2020-10-14 15:01:41 +0200

    ok

--- textfields.py
+++ textfields.py
@@ -2,30 +2,33 @@ from kivy.lang import Builder
 
 from kivymd.app import MDApp
 
+from bizLogic import BizLogic
+
 KV = '''
 BoxLayout:
     padding: "10dp"
 
+    MDLabel:
+        text: "GGT-Berechnung"
+        pos_hint: {"center_y": .9}
+
     MDTextField:
         id: text_input_1
         hint_text: "Erste Zahl (press 'Enter')"
-        helper_text: "There will always be a mistake"
+        helper_text: "integer values allowed only"
         helper_text_mode: "on_error"
         pos_hint: {"center_y": .8}
 
     MDTextField:
         id: text_input_2
         hint_text: "Zweite Zahl (press 'Enter')"
-        helper_text: "There will always be a mistake"
+        helper_text: "integer values allowed only"
         helper_text_mode: "on_error"
         pos_hint: {"center_y": .8}
 
     MDLabel:
         text: "(Ergebnis)"
-
-    MDRaisedButton:
-        text: "RUN"
-        md_bg_color: 0, 0, 1, 1
+        pos_hint: {"center_y": .2}
 '''
 
 
@@ -37,11 +40,11 @@ class Test(MDApp):
     def build(self):
         self.screen.ids.text_input_1.bind(
             on_text_validate=self.set_error_message,
-            on_focus=self.set_error_message,
+            on_focus=self.do_action,
         )
         self.screen.ids.text_input_2.bind(
             on_text_validate=self.set_error_message,
-            on_focus=self.set_error_message
+            on_focus=self.do_action,
         )
         return self.screen
 
@@ -49,5 +52,10 @@ class Test(MDApp):
         self.screen.ids.text_input_1.error = True
         self.screen.ids.text_input_2.error = True
 
+    def do_action(self):
+        try:
+            self.label_results.text = BizLogic.euklid(self.text_input1.text, self.text_input2.text)
+        except:
+            print("Abortion.")
 
 Test().run()
