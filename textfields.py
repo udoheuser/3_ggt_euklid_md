e9ca3f7cb3d51732a797ba86868c7eeb1d6353cd
udoheuser <heuser@dhbw-stuttgart.de>
2020-10-15 12:00:31 +0200

    not quite yet

--- textfields.py
+++ textfields.py
@@ -1,6 +1,8 @@
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
+from kivymd.uix.behaviors import TouchBehavior
+from kivymd.uix.button import MDRaisedButton
 
 from bizLogic import BizLogic
 
@@ -26,6 +28,12 @@ BoxLayout:
         helper_text_mode: "on_error"
         pos_hint: {"center_y": .8}
 
+    MyButton:
+        id: button1
+        text: "Run"
+        md_bg_color: 0, 0, 1, 1
+        pos_hint: {"center_y": .2}
+
     MDLabel:
         text: "(Ergebnis)"
         pos_hint: {"center_y": .2}
@@ -39,13 +47,13 @@ class Test(MDApp):
 
     def build(self):
         self.screen.ids.text_input_1.bind(
-            on_text_validate=self.set_error_message
-            # on_focus=self.on_text,
+            on_text_validate=self.set_error_message,
+            on_focus=self.on_text
             # on_text=self.screen.ids.text_input_1.text
         )
         self.screen.ids.text_input_2.bind(
-            on_text_validate=self.set_error_message
-            # on_focus=self.on_text
+            on_text_validate=self.set_error_message,
+            on_focus=self.on_text
             # on_text=self.screen.ids.text_input_2.text
         )
         return self.screen
@@ -60,5 +68,20 @@ class Test(MDApp):
             self.label_results.text = BizLogic.euklid(self.text_input1.text, self.text_input2.text)
         except:
             print("Abortion.")
+
+
+class MyButton(MDRaisedButton, TouchBehavior):
+    def on_long_touch(self, *args):
+        print("<on_long_touch> event")
+        self.label_results.text = "hello"
+
+    def on_double_tap(self, *args):
+        print("<on_double_tap> event")
+        self.label_results.text = "hello"
+
+    def on_triple_tap(self, *args):
+        print("<on_triple_tap> event")
+        self.label_results.text = "hello"
+
 
 Test().run()
